0a744a3f8dd31c8c393a57609eb24de4fa7eba38
DOMO9293 <flqjsl@gmail.com>
2020-06-30 22:49:04 +0200

    counter reducer added

---
+++ redux/counter/counter.reducer.js
@@ -0,0 +1,23 @@
+import counterActionTypes from "./counter.types";
+
+const INITAIL_STATE = {
+  counter: 0,
+};
+
+export default counterReducer = (state = INITAIL_STATE, action) => {
+  switch (action.type) {
+    case counterActionTypes.INCREASE_COUNTER:
+      return {
+        ...state,
+        counter: state.counter + 1,
+      };
+    case counterActionTypes.DECREASE_COUNTER:
+      return {
+        ...state,
+        counter: state.counter - 1,
+      };
+
+    default:
+      return state;
+  }
+};
